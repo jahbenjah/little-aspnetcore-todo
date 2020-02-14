953add512af4810d69b0889c5db322952a48d39b
Juan Benjamín Camacho Castro <benjamin.camacho@geniussoft.com.mx>
2020-02-14 10:31:09 -0600

    Se actualiza HomeController a  3.1

--- AspNetCoreTodo/Controllers/HomeController.cs
+++ AspNetCoreTodo/Controllers/HomeController.cs
@@ -9,26 +9,18 @@ using AspNetCoreTodo.Models;
 namespace AspNetCoreTodo.Controllers
 {
     public class HomeController : Controller
-    {
+    {      
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Privacy()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
